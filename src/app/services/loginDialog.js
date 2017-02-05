3b19867a09b484da2a5f09306afe1615b4f522ee
Adrian Panella <ianchi74@outlook.com>
2017-02-04 22:18:54 -0600

    loginDialog: many fixes
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- src/app/services/loginDialog.js
+++ src/app/services/loginDialog.js
@@ -4,7 +4,7 @@
  * @module amFramework
  *
  * @description
- * Provides authentication services
+ * Provides authentication services with http interceptor
  *
  *
  */
@@ -13,18 +13,18 @@
 angular
 	.module('amFramework')
 	.provider('amfLoginDialog', LoginDialogProvider)
-	.config(loginDialogConfig)
-	.run();
+	.config(loginDialogConfig);
 
 function LoginDialogProvider() {
 	// private vars
-	var username = null;
-	var session = null;
+	var loggedToken = null;
 	var options = {
-		loginCB: null,
+		loginFactory: null,
+		retryFilter: null,
 		httpErrors: ['401'],
 		jsonrpcErrors: [-32002],
-		cookie: 'sessionid'
+		cookie: 'auth-token',
+		disabled: false
 	};
 	var httpBuffer = [];
 
@@ -35,23 +35,35 @@ function LoginDialogProvider() {
 	// service factory
 	this.$get = loginDialogFactory;
 
+	// function definitions
 	function configure(opts) {
 		angular.extend(options, opts);
+
+		if (angular.isObject(options.cookie)) {
+			options.cookie.name = options.cookie.name || 'auth-token';
+			options.cookie.path = options.cookie.path || '/';
+		} else if (angular.isString(options.cookie)) {
+			options.cookie = { name: options.cookie, path: '/' };
+		} else
+			options.cookie = null;
 	}
 
-	function loginDialogFactory() {
+	function loginDialogFactory($injector) {
+		if (options.loginFactory && !options.loginCB)
+			options.loginCB = $injector.invoke(options.loginFactory);
+
 		return {
-			getUser: function() {
-				return username;
+			enable: function() {
+				options.disabled=false;
 			},
-			getSession: function() {
-				return session;
+			disable: function() {
+				options.disabled=true;
 			}
 		};
 	}
 
 	/* @ngInject */
-	function loginInterceptor($q, $injector) {
+	function loginInterceptor($q, $injector, $cookies, $rootScope, $amfHttpProgress) {
 		var $http, $mdDialog;
 
 		return {
@@ -62,21 +74,22 @@ function LoginDialogProvider() {
 
 		function interceptResponse(response) {
 			var config = response.config || {};
-			var data = response.data || {};
+			var data = angular.isArray(response.data) ? response.data : [response.data || {}];
 			var deferred = $q.defer();
 
-			if (!config.ignoreAuthModule && options.jsonrpcErrors.length &&
-			    data.error && data.jsonrpc === '2.0') {
-				if (options.jsonrpcErrors.indexOf(data.error)>=0) {
-					httpBuffer.push({
-						config: config,
-						deferred: deferred
-					});
+			if (!options.disabled && !config.ignoreAuthModule && options.jsonrpcErrors.length) {
+				for (var i=0; i< data.length; i++) {
+					if (data[i].error && data[i].jsonrpc === '2.0') {
+						if (options.jsonrpcErrors.indexOf(data[i].error.code) >= 0) {
+							config.deferred = deferred;
+							httpBuffer.push(config);
 
-					if (httpBuffer.length === 1)
-						showLoginDialog().then(retryAll, rejectAll);
+							if (httpBuffer.length === 1)
+								doLogin().then(retryAll, rejectAll);
 
-					return deferred.promise;
+							return deferred.promise;
+						}
+					}
 				}
 			}
 			return response;
@@ -86,15 +99,13 @@ function LoginDialogProvider() {
 			var config = rejection.config || {};
 			var deferred = $q.defer();
 
-			if (!config.ignoreAuthModule && options.httpErrors.length) {
+			if (!options.disabled && !config.ignoreAuthModule && options.httpErrors.length) {
 				if (options.httpErrors.indexOf(rejection.status)>=0) {
-					httpBuffer.push({
-						config: config,
-						deferred: deferred
-					});
+					config.deferred = deferred;
+					httpBuffer.push(config);
 
 					if (httpBuffer.length === 1)
-						showLoginDialog().then(retryAll, rejectAll);
+						doLogin().then(retryAll, rejectAll);
 
 
 					return deferred.promise;
@@ -107,10 +118,11 @@ function LoginDialogProvider() {
 		function retryAll() {
 			$http = $http || $injector.get('$http');
 			for (var i = 0; i < httpBuffer.length; ++i) {
-				$http(httpBuffer[i].config).then(function(res) {
-					httpBuffer[i].deferred.resolve(res);
+				if (options.retryFilter) options.retryFilter(httpBuffer[i], loggedToken);
+				$http(httpBuffer[i]).then(function(res) {
+					res.config.deferred.resolve(res);
 				}, function(rej) {
-					httpBuffer[i].deferred.reject(rej);
+					rej.config.deferred.reject(rej);
 				});
 			}
 			httpBuffer = [];
@@ -118,22 +130,98 @@ function LoginDialogProvider() {
 
 		function rejectAll(reason) {
 			for (var i = 0; i < httpBuffer.length; ++i) {
-				httpBuffer.deferred.reject(reason);
+				httpBuffer[i].deferred.reject(reason);
 			}
 			httpBuffer = [];
 		}
 
+		function doLogin() {
+			var deferred = $q.defer();
+
+			if (options.loginFactory && !options.loginCB)
+				options.loginCB = $injector.invoke(options.loginFactory);
+
+			// if there is no CB assume we are authorized
+			if ( typeof(options.loginCB) != 'function')
+				deferred.resolve(true);
+
+			// if this is the first time, try with cookie token
+			else if (!loggedToken && options.cookie &&
+				(loggedToken = $cookies.getObject(options.cookie.name))) {
+				options.loginCB(loggedToken)
+					.then(function(token) {
+						loggedToken = token;
+
+						if (options.cookie) {
+							$cookies.putObject(options.cookie.name, token, { path: options.cookie.path });
+						}
+						$rootScope.$broadcast('session.setup', token);
+						deferred.resolve(token);
+					}, function(errMsg) {
+						if (options.cookie)
+							$cookies.remove(options.cookie.name, { path: options.cookie.path });
+						deferred.resolve(showLoginDialog());
+					});
+			} else
+				deferred.resolve(showLoginDialog());
+
+			return deferred.promise;
+		}
+
 		function showLoginDialog() {
 			$mdDialog = $mdDialog || $injector.get('$mdDialog');
+
+			var oldState = $amfHttpProgress.state(false);
+
 			return $mdDialog.show({
 				templateUrl: 'app/services/loginDialog.tmpl.html',
+				controller: LoginDialogController,
 				controllerAs: 'dialog',
 				bindToController: true,
 				clickOutsideToClose: false,
 				escapeToClose: false,
 				fullscreen: true
 
+			}).finally(function() {
+				$amfHttpProgress.state(oldState);
 			});
+		}
+
+		/* ngInject */
+		function LoginDialogController($mdDialog, $cookies, $rootScope) {
+			var dialog = this;
+			dialog.username = '';
+			dialog.password = '';
+			dialog.error = false;
+
+			dialog.handleSubmit = handleSubmit;
+			dialog.handleCancel = handleCancel;
+
+			function handleSubmit() {
+				if (typeof(options.loginCB) == 'function') {
+					options.loginCB(dialog.username, dialog.password)
+						.then(function(token) {
+							loggedToken = token;
+							dialog.password = '';
+							dialog.username = '';
+							$mdDialog.hide(token);
+
+							if (options.cookie) {
+								$cookies.putObject(options.cookie.name, token, { path: options.cookie.path });
+							}
+							$rootScope.$broadcast('session.setup', token);
+						}, function(errMsg) {
+							if (options.cookie)
+								$cookies.remove(options.cookie.name, { path: options.cookie.path });
+							dialog.error=errMsg || 'Invalid username/password';
+						});
+				} else $mdDialog.hide();
+				return;
+			}
+
+			function handleCancel() {
+				return $mdDialog.cancel();
+			}
 		}
 	}
 }
@@ -143,3 +231,5 @@ function loginDialogConfig($httpProvider, amfLoginDialogProvider) {
 	// initialization
 	$httpProvider.interceptors.push(amfLoginDialogProvider._loginInterceptor);
 }
+
+
